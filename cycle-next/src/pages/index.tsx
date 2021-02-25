8404f59f8cf682b23c79f8a2a91493442929af46
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-02-25 02:59:38 -0300

    Finalizando terceiro dia

--- cycle-next/src/pages/index.tsx
+++ cycle-next/src/pages/index.tsx
@@ -1,19 +1,24 @@
 import React from "react";
+import Head from 'next/head';
+
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import ExperienceBar from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import styles from "../styles/pages/Home.module.css";
+import { ChallengeBox } from '../components/ChallengeBox';
 
-import Head from 'next/head';
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
       <Head>
         <title>Inicio | Cycle</title>
       </Head>
+
       <ExperienceBar />
+
       <section>
         <div className="">
           <Profile />
@@ -21,7 +26,10 @@ export default function Home() {
           <Countdown />
         </div>
 
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
+
       </section>
     </div>
   )
