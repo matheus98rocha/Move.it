51b58d359e761f19e0835bf67d16cc0dbae1a1bd
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-02-27 01:04:16 -0300

    Finalizando NLW

---
+++ moveIt-next/src/components/LevelUpModal.tsx
@@ -0,0 +1,20 @@
+import styles from '../styles/components/LevelUpModa.module.css';
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+export function LevelUpModal() {
+    const { level, closeLevelUpModal } = useContext(ChallengesContext);
+    return (
+        <div className={styles.overlay}>
+            <div className={styles.container}>
+                <header>{level}</header>
+
+                <strong>Parabéns</strong>
+                <p>Você alcançou um novo level</p>
+
+                <button type="button">
+                    <img src="/icons/close.svg" alt="Fechar modal" onClick={closeLevelUpModal} />
+                </button>
+            </div>
+        </div>
+    )
+}
--- moveIt-next/src/components/Profile.tsx
+++ moveIt-next/src/components/Profile.tsx
@@ -1,5 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 export function Profile() {
+
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/matheus98rocha.png" alt="Matheus Gonçalves" />
@@ -7,7 +13,7 @@ export function Profile() {
                 <strong>Matheus Gonçalves</strong>
                 <p>
                     <img src="icons/level.svg" alt="level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
